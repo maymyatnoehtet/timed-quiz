251af5b4bbac3ce53d927a0fd7d9a983f6521e8e
maymyatnoehtet <may.mn.htet@gmail.com>
2023-03-17 18:59:59 +1100

    Quiz start page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,23 @@
-var containerElement = document.querySelector(".container");
+var headerEl = document.querySelector("header");
+var startButtonEl = document.querySelector(".start-button");
+var textEl = document.querySelector(".text-hello");
 
-/* Landing page */
-/*
- * Start Quiz page
- */
-var landingHeaderElement = document.createElement("h1");
-landingHeaderElement.textContent = "Coding Quiz Challenge";
-landingHeaderElement.setAttribute("style", "display: flex; justify-content: center; margin-left: 5em; font-size: 2em; line-weight: bolder;");
-containerElement.appendChild(landingHeaderElement);
+var timer;
+var timerCount;
 
-var quizRuleElement = document.createElement("p");
-quizRuleElement.textContent = "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!";
-quizRuleElement.setAttribute("style", "display: flex; justify-content: center; text-align: center; font-size: 1.2em;");
-containerElement.appendChild(quizRuleElement);
+function startQuiz(){
+    console.log("start button is clicked");
+    console.log(headerEl.childNodes);
+    clearStartPage();
+    textEl.textContent = "Hola!Quiz Start.";
+}
 
+function clearStartPage(){
+    var headerChildrenLen = headerEl.childNodes.length;
+    console.log(headerChildrenLen);
+    for (let index = 0; index < headerChildrenLen; index++) {
+        headerEl.remove(headerEl.children[index]);
+    }
+}
 
-var buttonElement = document.createElement("button");
-buttonElement.textContent = "Start Quiz";
-buttonElement.setAttribute("style", "position: absolute; left: 50%; padding: 10px 20px; border-radius: 5px; background-color: #4b0082; color: white; font-size: 1.2em;");
-containerElement.appendChild(buttonElement);
-
-// function startQuiz() {
-//     removeElements(containerElement);
-//     var h1Element = document.createElement("h1");
-//     h1Element.textContent = "Halo! Let's start the quiz."
-//     containerElement.appendChild(h1Element);
-// }
-
-// function removeElements(element){
-//     console.log(element);
-//     childNodeLength(element);
-//     // while(element.children[i] !== NaN) {
-//     //     element.removeChild(element.children[i]);
-//     //     i++;
-//     // }
-// }
-
-// function childNodeLength(element) {
-//     var numChild = 0;
-//     while(element.children[numChild] !== NaN) {
-//         numChild += 1;
-//     } 
-//     console.log(numChild);
-//     return numChild;
-// }
-
-// addEventListener("click", startQuiz);
+startButtonEl.addEventListener("click", startQuiz);
